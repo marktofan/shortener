d0dc98c34e1bfee3f60f57e7aa96538874335e26
Petru Ilies <petru.ilies@softvision.ro>
2012-11-28 18:10:00 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortner::Application.config.secret_token = '07253361d62d9c0c52beb2b659ea047d16b652fd7d64262d551834e26307bb77fc1926452755627f8dcfadaae22d0d4009d98854f864cbfdd801aafc8409d948'
