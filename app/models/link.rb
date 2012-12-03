444cf1eb73d4e3c19fdef1abd3830f2e2e6f0596
Petru Ilies <petru.ilies@softvision.ro>
2012-12-03 17:36:06 +0200

    improvements on links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,28 @@
 class Link < ActiveRecord::Base
-  attr_accessible :id, :url
+  include FriendlyId
+  attr_accessible :url, :slug
+
+  friendly_id do |config|
+    config.base = :generate_short_url
+    config.use :slugged
+    config.sequence_separator = ""
+  end
+
+
+  validates :url, :presence => true, :uri => {:format => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix}
+
+
+  # override friendly id method to don't parameterize the field
+  def normalize_friendly_id(string)
+    string.to_s
+  end
+
+  def generate_short_url
+
+    hash= Digest::SHA1.hexdigest self.url
+    hash = Base64.encode64 hash
+    self.slug = hash[0..7]
+  end
+
+
 end
