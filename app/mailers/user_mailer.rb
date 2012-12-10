e7ed6e69240b4967af1cb80838997c8b21ba258f
Petru Ilies <petru.ilies@softvision.ro>
2012-12-10 13:43:48 +0200

    Add mailer and view + layout and gmail settings for development

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ActionMailer::Base
+  default from: "petru.ilies@softvision.ro"
+  layout 'mailer'
+
+  def welcome(user)
+
+    @user = user
+    @url = "http://example.com/sign_in"
+
+
+    mail(:to => user.email, :subject => "Welcome to Shortener")
+
+  end
+
+
+end
